bb1c7235e27b1be8f029aa4734adf33800bde49e
Bartek Wojdan <bwojdan@gmail.com>
2014-12-09 19:01:31 +0100

    Pushed project to repo.

---
+++ iPulse/iPulse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPulse
+//
+//  Created by Wojdan on 09.12.2014.
+//  Copyright (c) 2014 wojdan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iPulse/iPulse/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iPulse
+//
+//  Created by Wojdan on 09.12.2014.
+//  Copyright (c) 2014 wojdan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
